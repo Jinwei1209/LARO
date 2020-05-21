553d0569f18ca8e997479f0b07c5022ab4d21558
Jiahao Li <harry@jls-MacBook-Pro.local>
2020-05-21 17:22:30 -0400

    update presampled data

--- main_BO.py
+++ main_BO.py
@@ -14,7 +14,7 @@ from bayesOpt.bayes_opt_policies import *
 
 
 def policy_update(x, y, bounds, objective, n_iters, best, a_best, b_best,
-                policy_func, value_fig_name, mask_fig_name):
+                policy_func, value_fig_name, mask_fig_name, Plot = False):
 
     for i in range(n_iters):
         new_point, new_value = policy_func(train_x = x, train_y = y, bounds = bounds, objective = objective)
@@ -31,22 +31,23 @@ def policy_update(x, y, bounds, objective, n_iters, best, a_best, b_best,
         print('Iteration {:2d}, best value={:0.3f}'.format(i, best[-1]))
         print()
 
-    plt.figure()
-    plt.plot(best,'o-')
-    plt.xlabel('Iteration')
-    plt.ylabel('Best value found')
-    plt.savefig(value_fig_name)
-    plt.close()
+    if Plot:
+        plt.figure()
+        plt.plot(best,'o-')
+        plt.xlabel('Iteration')
+        plt.ylabel('Best value found')
+        plt.savefig(value_fig_name)
+        plt.close()
 
-    p_pattern = gen_pattern(10**a_best, 10**b_best, r_spacing=3)
-    # p_pattern = gen_pattern(a_best, b_best, r_spacing=3)
-    u = np.random.uniform(0, np.mean(p_pattern)/sampling_ratio, size=(256, 192))
-    masks = p_pattern > u
-    masks[128-13:128+12, 96-13:96+12] = 1
-    plt.figure()
-    plt.imshow(masks)
-    plt.savefig(mask_fig_name)
-    plt.close()
+        p_pattern = gen_pattern(10**a_best, 10**b_best, r_spacing=3)
+        # p_pattern = gen_pattern(a_best, b_best, r_spacing=3)
+        u = np.random.uniform(0, np.mean(p_pattern)/sampling_ratio, size=(256, 192))
+        masks = p_pattern > u
+        masks[128-13:128+12, 96-13:96+12] = 1
+        plt.figure()
+        plt.imshow(masks)
+        plt.savefig(mask_fig_name)
+        plt.close()
 
 
 
@@ -56,12 +57,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BO-LOUPE')
     parser.add_argument('--gpu_id', type=str, default='0')
     parser.add_argument('--flag_policy', type=int, default=0)  # 0 for EI, 1 for KG
+    parser.add_argument('--cross_val', type=int, default=0)    # 0 for not doing cross-validation, 1 for doing cross-validation
     opt = {**vars(parser.parse_args())}
     # fixed parameters
-    q = 1  # number of lookahead
+    q = 1  # number of step lookahead
     contrast = 'T1'
     sampling_ratio = 0.1
-    n_pre_samples = 3
+    n_pre_samples = 15
     n_iters = 30
 
     os.environ['CUDA_VISIBLE_DEVICES'] = opt['gpu_id']
@@ -70,7 +72,6 @@ if __name__ == '__main__':
 
     rootName = '/data/Jinwei/{0}_slice_recon_GE'.format(contrast)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     data_loader = kdata_loader_GE(
             rootDir=rootName,
             contrast=contrast, 
@@ -83,21 +84,30 @@ if __name__ == '__main__':
 
     bounds = np.array([[-1., -1.], [1., 1.]])  # for log uniform space
     # bounds = np.array([[0., 0.], [10., 10.]])  # for uniform space
-
-    print('device: {}'.format(device))
-
     objective = lambda z: recon_loss(z, data_loader, device, sampling_ratio)
 
-    x_list, y_list = [], []
-    # random initialization
-    for params in np.random.uniform(bounds[0], bounds[1], (n_pre_samples, bounds.shape[0])):
-            x_list.append(params)
-            y_list.append(objective(params))
-    
-    x = np.array(x_list)
-    y = np.array(y_list)
-    print('Value of best point found: {}'.format(y.max()))
+    # Read in data from a file.
+    filename = 'presample_data.csv'
+    # If data doesn't exist, generate it
+    if ~os.path.exists(filename):
+        np.random.seed(1)
+        x_list, y_list = [], []
+        # random initialization
+        for params in np.random.uniform(bounds[0], bounds[1], (n_pre_samples, bounds.shape[0])):
+                x_list.append(params)
+                y_list.append(objective(params))
+        x = np.array(x_list)
+        y = np.array(y_list)
+        data = np.transpose([x,y])
+        np.savetxt(filename,data)
 
+    # Read in data from a file.  
+    data = np.loadtxt(filename)
+    x = data[:,0] # First column of the data
+    y = data[:,1] # Second column of the data
+
+    
+    print('Value of best point found: {}'.format(y.max()))
     a_best, b_best = x[np.argmax(y)]
     best = [y.max()] # This will store the best value
 
@@ -105,12 +115,13 @@ if __name__ == '__main__':
         value_fig_name = 'Values_EI.png'
         mask_fig_name = 'mask_best_EI.png'
         policy_update(x, y, bounds, objective, n_iters, best, a_best, b_best,
-                        EI_policy, value_fig_name, mask_fig_name)
+                        EI_policy, value_fig_name, mask_fig_name, True)
+        cross_validation()
     else:
         value_fig_name = 'Values_KG.png'
         mask_fig_name = 'mask_best_KG.png'
         policy_update(x, y, bounds, objective, n_iters, best, a_best, b_best,
-                        KG_policy, value_fig_name, mask_fig_name)
+                        KG_policy, value_fig_name, mask_fig_name, True)
 
 
 
